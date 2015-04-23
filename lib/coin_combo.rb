76fc6fbe587836ad04952a37059ee4c0784de25b
Chris Sefkin & Jessica Engel <student@epicodus.com>
2015-04-23 11:18:19 -0700

    project returns numbreturns number of quarters, dimes, nickels and pennies that input is div by

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -1,17 +1,31 @@
-class Float
+class Fixnum
 
   define_method(:coin_combo) do
 
 input=self
-output=0.0
-result= 0
+quarter = 0
+dime = 0
+nickel = 0
+penny = 0
 
-until output==input
-  if output.%(0.25)==0.0
-    output=output.+0.25
-    result=result+1
+until input==(0)
+  if input.>=(25)
+    var1=input./(25)
+    quarter=var1.floor()
+    input=input.-(quarter.*(25))
+  elsif input.>=(10)
+    var2 = input./(10)
+    dime = var2.floor()
+    input=input.-(dime.*(10))
+  elsif input.>=(5)
+    var3 = input./(5)
+    nickel = var3.floor()
+    input = input.-(nickel.*(5))
+  elsif input.>=1
+    penny = input./(1)
+    input = input.-(penny.*(1))
   end
 end
-result
+return [quarter, dime, nickel, penny]
 end
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -1,8 +1,19 @@
 require ('rspec')
 require ('coin_combo')
 
-describe('Float#coin_combo') do
+describe('Fixnum#coin_combo') do
   it("returns number of quarters that input is divisible by") do
-    expect((1.00).coin_combo()).to(eq(4))
+    expect((100).coin_combo()).to(eq([4, 0, 0, 0]))
+end
+    it("returns number of dimes and quarters that input is divisible by") do
+      expect((110).coin_combo()).to(eq([4, 1, 0, 0]))
   end
+
+    it("returns number of quarters, dimes and nickels that input is div by") do
+      expect((115).coin_combo()).to(eq([4, 1, 1, 0]))
+end
+
+    it("returns number of quarters, dimes, nickels and pennies that input is div by") do
+      expect((118).coin_combo()).to(eq([4, 1, 1, 3]))
+end
 end
